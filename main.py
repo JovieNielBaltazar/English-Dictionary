4a50bf92f4d2713d08158d812e769319fab797c7
JovieNielBaltazar <jovienielbaltazar@gmail.com>
2022-09-13 11:56:52 -0700

    added search function

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+data = json.load(open("data.json"))
+
+def search_for_word(input1):
+    if input1 in data:
+        for item in range(len(data[input1])):
+            print(data[input1][int(item)-1])
+    else:
+        print("the word cannot be found, please try again!")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+input1 =input("Enter word to be searched: ")
+search_for_word(input1)
